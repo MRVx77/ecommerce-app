63c94496d633d4e4baf6464dd564b18d22f23753
mrv <mrv@gmail.com>
2025-11-16 09:56:02 +0530

    add SearchBar component and integrate it into Navbar; update ShopContext for search functionality

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,5 +1,6 @@
 import js from "@eslint/js";
 import globals from "globals";
+import react from "eslint-plugin-react";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import { defineConfig, globalIgnores } from "eslint/config";
@@ -10,7 +11,8 @@ export default defineConfig([
     files: ["**/*.{js,jsx}"],
     extends: [
       js.configs.recommended,
-      reactHooks.configs.flat.recommended,
+      react.configs.flat.recommended,
+      //reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
     ],
     languageOptions: {
@@ -28,6 +30,7 @@ export default defineConfig([
         "off",
         { allowConstantExport: true },
       ],
+      "react-hooks/exhaustive-deps": "off",
     },
   },
 ]);
--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -5,7 +5,7 @@ import Title from "../components/Title";
 import ProductItem from "../components/ProductItem";
 
 const Collection = () => {
-  const { products } = useContext(ShopContext);
+  const { products, search, showSearch } = useContext(ShopContext);
   const [showFilter, setShowFilter] = useState(false);
   const [filterProducts, setFilterProducts] = useState([]);
   const [category, setCategory] = useState([]);
@@ -32,6 +32,11 @@ const Collection = () => {
 
   const applyFilter = () => {
     let productsCopy = products.slice();
+    if (showSearch && search) {
+      productsCopy = productsCopy.filter((item) =>
+        item.name.toLowerCase().includes(search.toLowerCase())
+      );
+    }
     if (category.length > 0) {
       productsCopy = productsCopy.filter((item) =>
         category.includes(item.category)
@@ -66,10 +71,8 @@ const Collection = () => {
   };
 
   useEffect(() => {
-    // eslint-disable-next-line react-hooks/set-state-in-effect
     applyFilter();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [category, subCategory, products]);
+  }, [category, subCategory, products, search, showSearch]);
 
   useEffect(() => {
     sortProducts();
