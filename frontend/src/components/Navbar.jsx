859f2400f1d19983c537cdf14f158d3a3ba6dae5
mrv <mrv@gmail.com>
2025-11-18 16:22:10 +0530

    implement Login page with form handling and toggle between Sign Up and Login states

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -40,11 +40,14 @@ const Navbar = () => {
         />
 
         <div className="group relative">
-          <img
-            src={assets.profile_icon}
-            alt=""
-            className="w-5 cursor-pointer"
-          />
+          <Link to={"/login"}>
+            <img
+              src={assets.profile_icon}
+              alt=""
+              className="w-5 cursor-pointer"
+            />
+          </Link>
+
           <div className="group-hover:block hidden absolute dropdown-menu right-0 pt-4">
             <div className="flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded">
               <p className="cursor-pointer hover:text-black">My Profile</p>
@@ -73,7 +76,7 @@ const Navbar = () => {
           className="w-5 cursor-pointer sm:hidden "
         />
       </div>
-      {/* sidebar menu for small screen */}
+      {/* -------------sidebar menu for small screen -------------*/}
       <div
         className={`absolute top-0 right-0 bottom-0 overflow-hidden bg-white transition-all ${
           visible ? "w-full" : "w-0"
