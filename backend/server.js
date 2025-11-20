de0c02ac890e5c03ee058b5746fa31eb9ec2ef88
mrv <mrv@gmail.com>
2025-11-20 20:56:05 +0530

    implement adim auth and for add and remove using token and started admin folder

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import { connect } from "mongoose";
 import connnectDb from "./config/mongodb.js";
 import connectCloudinary from "./config/cloudinary.js";
 import userRouter from "./routes/userRoute.js";
+import proudctRouter from "./routes/productRoutes.js";
 
 // App config
 const app = express();
@@ -18,6 +19,8 @@ app.use(cors());
 
 // api endpoints
 app.use("/api/user", userRouter);
+app.use("/api/product", proudctRouter);
+
 app.get("/", (req, res) => {
   res.send("api is running");
 });
